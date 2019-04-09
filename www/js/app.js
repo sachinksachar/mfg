54628b27d9e2cf78130904a7458710d6e246d346
sachin achar <sachinksacharya2gmail.com>
2019-04-09 22:17:53 +0530

    Login page changes

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,130 @@
-// Ionic Starter App
+var app = angular.module('mfg', ['ionic', 'ionic-material', 'oc.lazyLoad']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
-      StatusBar.styleDefault();
-    }
-  });
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+
+    .state('login', {
+        cache: false,
+        url: '/login',
+        templateUrl: 'templates/Login/signin.html',
+        controller: 'loginCtrl',
+        resolve: {
+          deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+            return $ocLazyLoad.load({
+              name: "mfg",
+              files: [
+                'js/components/authentication/loginController.js'
+              ]
+            });
+          }]
+        }
+  
+      })
+
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    // .state('app.login', {
+    //     cache: false,
+    //     url: '/login',
+    //     views: {
+    //         'menuContent': {
+    //             templateUrl: 'templates/Login/signin.html',
+    //             controller: 'loginCtrl',
+    //         }
+    //     },
+
+    //     resolve: {
+    //       deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+    //         return $ocLazyLoad.load({
+    //           name: "mfg",
+    //           files: [
+    //             'js/components/authentication/loginController.js'
+    //           ]
+    //         });
+    //       }]
+    //     }
+  
+    //   })
+
+    .state('app.lists', {
+        url: '/lists',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/lists.html',
+                controller: 'ListsCtrl'
+            }
+        }
+    })
+
+    .state('app.ink', {
+        url: '/ink',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/ink.html',
+                controller: 'InkCtrl'
+            }
+        }
+    })
+
+    .state('app.motion', {
+        url: '/motion',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/motion.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    .state('app.components', {
+        url: '/components',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/components.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+
+    .state('app.extensions', {
+        url: '/extensions',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/extensions.html',
+                controller: 'ExtensionsCtrl'
+            }
+        },
+        resolve: {
+            deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+              return $ocLazyLoad.load({
+                name: "mfg",
+                files: [
+                  'js/controllers/ExtensionsCtrl.js'
+                ]
+              });
+            }]
+          }
+    })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/components');
+});
