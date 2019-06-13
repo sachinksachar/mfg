1862112e9e0ce45dcfc6f01af7d5c5eec8ec5191
sachin achar <sachinksacharya2gmail.com>
2019-06-13 22:45:03 +0530

    profile create page UI work

--- www/js/components/ContestList/contestListController.js
+++ www/js/components/ContestList/contestListController.js
@@ -1,24 +1,27 @@
 angular.module('mfg').controller('contestListCtrl', ['$http', '$scope', '$state', '$stateParams', '$filter', '$ionicModal', '$ionicPopup', '$ionicLoading', '$ionicPlatform', '$timeout', '$ionicTabsDelegate', '$ionicSlideBoxDelegate', '$window',
     function ($http, $scope, $state, $stateParams, $filter, $ionicModal, $ionicPopup, $ionicLoading, $ionicPlatform, $timeout, $ionicTabsDelegate, $ionicSlideBoxDelegate, $window) {
 
-        
-
-        $scope.backButton = function(){
+        $scope.backButton = function () {
             $state.go('contestCategory')
         }
 
         $scope.local_match_data = JSON.parse(localStorage['local_match_data']);
         $scope.contestList = {};
         $http({
-            url: __env.apiUrl + '/api/cricket/contests/'+$scope.local_match_data.matchcode+'/'+$scope.local_match_data.levelcode,
+            url: __env.apiUrl + '/api/cricket/contests/' + $scope.local_match_data.matchcode + '/' + $scope.local_match_data.levelcode,
             method: "GET",
             headers: { 'Content-Type': 'application/json' }
-          }).then(function (response) { 
-            $scope.contestList = response.data;    
-          }, function (error) { 
-            
-      
-          });
+        }).then(function (response) {
+            $scope.contestList = response.data;
+        }, function (error) {
+
+
+        });
+
+        // create a team
+        $scope.createTeam = function () {
+            $state.go('newTeam');
+        }
 
 
     }])
--- www/js/components/Teams/newTeamController.js
+++ www/js/components/Teams/newTeamController.js
@@ -1,25 +1,48 @@
 angular.module('mfg').controller('newTeamCtrl', ['$http', '$scope', '$state', '$stateParams', '$filter', '$ionicModal', '$ionicPopup', '$ionicLoading', '$ionicPlatform', '$timeout', '$ionicTabsDelegate', '$ionicSlideBoxDelegate', '$window',
-    function ($http, $scope, $state, $stateParams, $filter, $ionicModal, $ionicPopup, $ionicLoading, $ionicPlatform, $timeout, $ionicTabsDelegate, $ionicSlideBoxDelegate, $window) {
+  function ($http, $scope, $state, $stateParams, $filter, $ionicModal, $ionicPopup, $ionicLoading, $ionicPlatform, $timeout, $ionicTabsDelegate, $ionicSlideBoxDelegate, $window) {
 
-        debugger
+    $scope.backButton = function () {
+      $state.go('contestCategory')
+    }
 
-        $scope.backButton = function(){
-            $state.go('contestCategory')
-        }
-
-        $scope.version_id = '1.0';
-        $scope.local_match_data = JSON.parse(localStorage['local_match_data']);
-        $scope.contestList = {};
-        $http({
-            url: __env.apiUrl + '/api/cricket/squad/rules/'+$scope.version_id,
-            method: "GET",
-            headers: { 'Content-Type': 'application/json' }
-          }).then(function (response) { debugger
-            $scope.contestList = response.data;    
-          }, function (error) { debugger
-            
-      
-          });
+    $scope.version_id = '1.0';
+    $scope.local_match_data = JSON.parse(localStorage['local_match_data']);
+    $scope.contestList = {};
+    $http({
+      url: __env.apiUrl + '/api/cricket/squad/rules/' + $scope.version_id,
+      method: "GET",
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (response) {
+      $scope.contestList = response.data;
+    }, function (error) {
 
 
-    }])
+    });
+
+    // players fetch
+    $scope.players = {};
+
+    $http({
+      url: __env.apiUrl + '/api/cricket/squad/' + $scope.local_match_data.matchcode,
+      method: "GET",
+      headers: { 'Content-Type': 'application/json' }
+    }).then(function (response) {
+      $scope.players = response.data;
+    }, function (error) {
+
+
+    });
+
+    // Sliding
+
+    $scope.activeSlide = 0;
+    $scope.slideChanged = function (index) {
+      $ionicTabsDelegate.select(index);
+    };
+
+    $scope.setSlide = function (index) {
+      $ionicSlideBoxDelegate.slide(index);
+    };
+
+
+  }])
