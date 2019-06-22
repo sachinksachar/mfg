b0b220a5c18e3a1fb35e6464a8b9104147bea722
sachin achar <sachinksacharya2gmail.com>
2019-06-22 16:34:33 +0530

    register and login error handling

--- www/js/controllers/AppCtrl.js
+++ www/js/controllers/AppCtrl.js
@@ -1,4 +1,4 @@
-﻿app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout, $http) {
+﻿app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout, $http, $ionicHistory, $window, $state, $ionicLoading) {
     $scope.games = {};
 
     $http.defaults.headers.common.Authorization = 'Bearer ' + localStorage.token;
@@ -20,6 +20,40 @@
 
       $scope.changeGame = function(game){
         
+      }
+
+
+      $scope.logoutAction = function(){
+
+        $ionicLoading.show({
+          content: 'Loading',
+          template: '<ion-spinner icon="lines" class="spinner-assertive"></ion-spinner>',
+          animation: 'fade-in',
+          showBackdrop: true,
+          maxWidth: 200,
+          showDelay: 0
+        });
+
+        $scope.logoutdate = {}
+        $http({
+          url: __env.apiUrl + '/auth/logout',
+          method: "GET",
+          headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) { 
+          $scope.logoutdate = response.data.status;
+          $window.localStorage.clear();
+          $ionicHistory.clearCache();
+          $ionicHistory.clearHistory();
+          
+          $timeout(function(){
+            $ionicLoading.hide()
+            $state.go('login');
+          }, 1000)
+         
+        }, function (error) {
+          
+    
+        });
       }
 
 
